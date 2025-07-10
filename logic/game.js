be29ba92fa1d400e925231766deeb6601df30cd5
andrian crestian <andriancrestian@dedagroup.it>
2025-07-10 15:26:59 +0200

    check when snake attempt going backwards and ignore

--- logic/game.js
+++ logic/game.js
@@ -1,32 +1,48 @@
 import { drawSnake, clearCanvas, drawText } from "./drawing.js";
 import { ORIGINAL_SNAKE, STARTING_SCORE } from "./config.js";
 import { gameState } from "./gameState.js";
+import { isSameCartesianPoint } from "./utility.js";
 
 export function moveSnake(direction, snake) {
+    console.log("current head position", snake[0])
     const currentSnake = [...gameState.snake];
     let newHead = null;
     const currentHead = snake[0];
+    const secondPosition = snake.length > 1 ? { x: snake[1].x, y: snake[1].y } : null;
+    console.log("second", secondPosition)
+
     switch (direction) {
+        //going up, decrease the y axis
         case ("up"):
             newHead = { x: currentHead.x, y: currentHead.y - 10 };
             break;
+        //going down, increase the y axis
         case ("down"):
             newHead = { x: currentHead.x, y: currentHead.y + 10 };
             break;
+        //going right, increase the x axis
         case ("right"):
             newHead = { x: currentHead.x + 10, y: currentHead.y };
             break;
+        //going left, decrease the x axis
         case ("left"):
             newHead = { x: currentHead.x - 10, y: currentHead.y };
             break;
         default:
             break;
     }
-    //direction not valid
+    //direction keyboard key not valid
     if (!newHead) {
-        console.error("Direction not valid!")
+        console.error("Direction not valid!");
+        return;
     };
-    //position overlapping with the snake 
+    //going backwards not allowed
+    if (secondPosition && isSameCartesianPoint(newHead, secondPosition)) {
+        console.error("Going backwards is not allowed!");
+        return;
+    }
+
+    //collision with snake detected
     const snakeCollison = currentSnake.some((segment) => segment.x === newHead.x && segment.y === newHead.y);
     if (snakeCollison) {
         console.error("Collision with snake detected!");
@@ -39,7 +55,10 @@ export function moveSnake(direction, snake) {
         gameOver();
         return;
     }
+    //remind intercept eat food
+    //check if new snake causing any collision
 
+    //movement accepted, drawing the new snake
     let newSnake = [newHead];
     for (let i = 0; i < snake.length - 1; i++) {
         const segment = { x: snake[i].x, y: snake[i].y }
@@ -50,7 +69,8 @@ export function moveSnake(direction, snake) {
 
     //drawing newSnake
     clearCanvas();
-    console.log("New head position x: ", newHead.x, " y: ", newHead.y)
+    console.log("New head position x: ", newHead.x, " y: ", newHead.y);
+    console.log("----------------------")
     drawSnake(gameState.snake);
 }
 export function gameOver() {
