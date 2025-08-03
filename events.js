6af296ef3cacd93287711925d5ea9adafd5537a6
crestianiam <andrian.crestian95@gmail.com>
2025-08-03 12:46:57 +0200

    events handler changed

--- events.js
+++ events.js
@@ -40,7 +40,7 @@ export function registerEventListeners() {
             (gameState.snake.length === 1 || !isOppositeDirection(newDirection, gameState.currentDirection))) {
             gameState.nextDirection = newDirection;
         }
-        //moveSnake(newDirection)
+        moveSnake(newDirection)
     });
 
     canvas.addEventListener('click', function (event) {
